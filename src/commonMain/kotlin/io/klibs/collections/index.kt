00263ac69c2910ade81ef03431c0ae1e851ae740
Elizabeth <foxcapade@gmail.com>
2023-02-09 23:22:51 -0500

    constructors

--- src/commonMain/kotlin/io/klibs/collections/index.kt
+++ src/commonMain/kotlin/io/klibs/collections/index.kt
@@ -33,9 +33,61 @@ fun <T> dequeOf(vararg values: T): Deque<T> {
   return out
 }
 
+fun byteDequeOf(vararg values: Byte): ByteDeque {
+  val out = ByteDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
+fun shortDequeOf(vararg values: Short): ShortDeque {
+  val out = ShortDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
+fun intDequeOf(vararg values: Int): IntDeque {
+  val out = IntDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
+fun longDequeOf(vararg values: Long): LongDeque {
+  val out = LongDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
 @OptIn(ExperimentalUnsignedTypes::class)
 fun ubyteDequeOf(vararg values: UByte): UByteDeque {
   val out = UByteDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+fun ushortDequeOf(vararg values: UShort): UShortDeque {
+  val out = UShortDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+fun uintDequeOf(vararg values: UInt): UIntDeque {
+  val out = UIntDeque(values.size)
+  for (v in values)
+    out += v
+  return out
+}
+
+@OptIn(ExperimentalUnsignedTypes::class)
+fun ulongDequeOf(vararg values: ULong): ULongDeque {
+  val out = ULongDeque(values.size)
   for (v in values)
     out += v
   return out
